e07adfe1e3aa7f8592663410ce441a269ba09fdf
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-08-26 12:00:14 +1000

    Update

--- Challenges/loops_challenges.m
+++ Challenges/loops_challenges.m
@@ -2,59 +2,23 @@
 % Loops
 
 %% Challenge #1
-% Using the worked example as a guide, write a script that will calculate
-% the sum of the inflammation of each patient over 40 days for the file
-% inflammation-01 starting from day 40 to day 1. Display the sum for each
-% day to the command line.
-
-
-% Change your loop so that it only displays the sums on days 4,7,13,16,28
-% and 33.
+% Using the worked example as a guide, write a script places each Pokemon on 
+% the map for 1 sec and then places the next pokemon on the same figure. 
+% Hint: check documentation for ?pause? function
+% Change your loop so that it only displays only pokemons 4,7,13,16,28
+% and 33 for a second and then closes the figure.
 
 %% Challenge 1 - Extension
-% Modify your loop to instead PLOT the inflammation data of ALL patients on days 4,7,13,16,28 and 33 
-% Use subplots with two rows and three columns
-% Make sure each of your subplot has a title that indicates the day.
 
-% Save this figure as a png file to disk.
+% Modify your loop to instead place multiple pokemons on the same figure. 
+% Hint: Open placePokemon function and study the for loop implemented in it. 
+% Save this figure as a png file to disk and tweet in! Don?t forget to use @ResPlat :D
+
 
 %% Challenge #2
 
 % Insert for loop so that the script loads in each data file sequentially, 
-% and saves out a heatmap for each data file (using imagesc).
-% HINT: loop using the variable idx.
-
-%% Challenge 3
-
-% Open pie_script2.m and debug it
-
-%% Challenge 3 - Extension
-
-% if you're feeling creative, add a pie chart to the code to show 
-% how much pie has been eaten! 
-%__________________________________________________________________________
-
-%% Challenge #1
-% Using the worked example as a guide, write a script that will calculate
-% the sum of the inflammation of each patient over 40 days for the file
-% inflammation-01 starting from day 40 to day 1. Display the sum for each
-% day to the command line.
-
-
-% Change your loop so that it only displays the sums on days 4,7,13,16,28
-% and 33.
-
-%% Challenge 1 - Extension
-% Modify your loop to instead PLOT the inflammation data of ALL patients on days 4,7,13,16,28 and 33 
-% Use subplots with two rows and three columns
-% Make sure each of your subplot has a title that indicates the day.
-
-% Save this figure as a png file to disk.
-
-%% Challenge #2
-
-% Insert for loop so that the script loads in each data file sequentially, 
-% and saves out a heatmap for each data file (using imagesc).
+% and saves out a figure of Pokemon placed on map.
 % HINT: loop using the variable idx.
 
 %% Challenge 3
